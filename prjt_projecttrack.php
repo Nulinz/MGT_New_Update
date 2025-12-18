f14b2894e89cdbbbfab3623a5dc07479fad7bf4a
Jeevi-developer <gvsivagiri@gmail.com>
2025-12-18 12:16:48 +0530

    Remove all files from repository

--- prjt_projecttrack.php
+++
@@ -1,8 +0,0 @@
-<div class="empdetails">
-    <div class="sidebodyhead">
-        <h4 class="m-0">Project Track Details</h4>
-    </div>
-    <div class="mt-3 listtable">
-        <?php include('./timeline.php'); ?>
-    </div>
-</div>
